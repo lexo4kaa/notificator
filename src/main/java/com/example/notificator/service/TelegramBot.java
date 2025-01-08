c9f3877ff4933724ab6fc112aefe183c8087e363
lexo4kaa <alesha.krupenko@gmail.com>
2025-01-08 17:18:13 +0300

    Add Menu
    Default commands have been added.
    
    jid: TG-3

--- src/main/java/com/example/notificator/service/TelegramBot.java
+++ src/main/java/com/example/notificator/service/TelegramBot.java
@@ -4,9 +4,15 @@ import com.example.notificator.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -17,6 +23,20 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot(BotConfig config) {
         super(config.getToken());
         this.config = config;
+        createCommands();
+    }
+
+    private void createCommands() {
+        List<BotCommand> listOfCommands = new ArrayList();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete data stored"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        try {
+            execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
     }
 
     @Override
